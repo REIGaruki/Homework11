c4b3b875308d389b4aeced55d2c546322494bfc9
REIGaruki <gyakujo@mail.ru>
2024-03-13 20:06:50 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int year = 2024;
         isThisYearLeap(year);
+        int clientOS = 3;
+        int clientDeviceYear = 2014;
+        System.out.println(setupInvitation(clientOS, clientDeviceYear));
     }
     public static void isThisYearLeap(int year) {
         if (year >= 1584 && year % 4 == 0 && year % 100 != 0  || year % 400 == 0) {
@@ -9,5 +12,18 @@ public class Main {
         } else {
             System.out.println("Год " + year + " не является високосным");
         }
+    }
+    public static String setupInvitation(int clientOS, int clientDeviceYear){
+        String system = "Вашего телефона";
+        String version = "версию";
+        if (clientOS == 0) {
+            system = "iOS";
+        } else if (clientOS == 1){
+            system = "Android";
+        }
+        if (clientDeviceYear < 2015) {
+            version = "облегченную версию";
+        }
+        return "Установите " + version + " приложения для " + system + " по ссылке:";
     }
 }
