f02616e28530ba6567eb7602d34b0d785b60a845
REIGaruki <gyakujo@mail.ru>
2024-03-13 19:48:47 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int year = 2024;
+        isThisYearLeap(year);
+    }
+    public static void isThisYearLeap(int year) {
+        if (year >= 1584 && year % 4 == 0 && year % 100 != 0  || year % 400 == 0) {
+            System.out.println("Год " + year + " является високосным");
+        } else {
+            System.out.println("Год " + year + " не является високосным");
+        }
     }
 }
